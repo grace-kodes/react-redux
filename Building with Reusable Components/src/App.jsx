9b65d129978a3812f5b0fa280ef6a43d7f493bb8
Grace Ko <gracekodes@gmail.com>
2024-12-17 20:33:31 -0800

    created components

---
+++ Building with Reusable Components/src/App.jsx
@@ -0,0 +1,14 @@
+import ProfileCard from './ProfileCard.jsx';
+
+function App() {
+    return (
+        <div>
+            <div>Personal Digital Assistants</div>
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+        </div>
+    )
+}
+
+export default App;
