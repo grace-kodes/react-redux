819f6468a287f43561cf0b3f4e52b0272e61f5ba
Grace Ko <gracekodes@gmail.com>
2024-12-17 21:05:47 -0800

    added props

--- Building with Reusable Components/src/App.jsx
+++ Building with Reusable Components/src/App.jsx
@@ -1,14 +1,17 @@
 import ProfileCard from './ProfileCard.jsx';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
     return (
         <div>
             <div>Personal Digital Assistants</div>
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+            <ProfileCard title="Alexa" handle="@alexca99" image={AlexaImage} />
+            <ProfileCard title="Cortana" handle="@Cortana32" image={CortanaImage} />
+            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
         </div>
-    )
+    );
 }
 
 export default App;
