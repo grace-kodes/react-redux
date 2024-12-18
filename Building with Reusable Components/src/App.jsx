68b17678e608947d90d02e07faec3304f97f93fa
Grace Ko <gracekodes@gmail.com>
2024-12-17 22:22:14 -0800

    added and implemented bulma for css

--- Building with Reusable Components/src/App.jsx
+++ Building with Reusable Components/src/App.jsx
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard.jsx';
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
@@ -7,9 +8,21 @@ function App() {
     return (
         <div>
             <div>Personal Digital Assistants</div>
-            <ProfileCard title="Alexa" handle="@alexca99" image={AlexaImage} />
-            <ProfileCard title="Cortana" handle="@Cortana32" image={CortanaImage} />
-            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard title="Alexa" handle="@alexca99" image={AlexaImage} />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="Cortana" handle="@Cortana32" image={CortanaImage} />
+                        </div> 
+                        <div className="column is-4">
+                            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} /> 
+                        </div> 
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
